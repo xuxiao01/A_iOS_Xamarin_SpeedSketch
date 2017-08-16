d863513ce68cfb841bc8d212feebb12a880b41e8
Cloudwu8888 <cloudwu8888@hotmail.com>
2017-08-16 14:34:24 +0800

    Add Analytics and Crashes SDK

--- SpeedSketch/AppDelegate.cs
+++ SpeedSketch/AppDelegate.cs
@@ -1,5 +1,8 @@
 ﻿using Foundation;
 using UIKit;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace SpeedSketch
 {
@@ -10,6 +13,8 @@ namespace SpeedSketch
 
 		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 		{
+			MobileCenter.Start("9d6d8829-627c-4ceb-815e-90ba99958eb8",
+				   typeof(Analytics), typeof(Crashes));
 			// Minimal basic setup without a storyboard.
 			var localWindow = new UIWindow (UIScreen.MainScreen.Bounds);
 			localWindow.RootViewController = new CanvasMainViewController ();

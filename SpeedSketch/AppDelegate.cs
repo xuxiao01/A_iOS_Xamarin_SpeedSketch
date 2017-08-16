5e66803e7520a8532e3720e1b7615d5e1aea5e60
Cloudwu8888 <cloudwu8888@hotmail.com>
2017-08-16 17:20:35 +0800

    Add push code and SDK, but it doesn't seem push successful

--- SpeedSketch/AppDelegate.cs
+++ SpeedSketch/AppDelegate.cs
@@ -3,6 +3,7 @@ using UIKit;
 using Microsoft.Azure.Mobile;
 using Microsoft.Azure.Mobile.Analytics;
 using Microsoft.Azure.Mobile.Crashes;
+using Microsoft.Azure.Mobile.Push;
 
 namespace SpeedSketch
 {
@@ -13,6 +14,39 @@ namespace SpeedSketch
 
 		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 		{
+			Push.PushNotificationReceived += (sender, e) =>
+			{
+
+				// Add the notification message and title to the message
+				var summary = $"Push notification received:" +
+
+						$"\n\tNotification title: {e.Title}" +
+						$"\n\tMessage: {e.Message}";
+
+				// If there is custom data associated with the notification,
+				// print the entries
+				if (e.CustomData != null)
+				{
+					summary += "\n\tCustom data:\n";
+					foreach (var key in e.CustomData.Keys)
+					{
+						summary += $"\t\t{key} : {e.CustomData[key]}\n";
+					}
+				}
+				UIAlertView alert = new UIAlertView()
+				{
+					Title = "alert title",
+					Message = summary
+				};
+				alert.AddButton("OK");
+				alert.Show();
+				// Send the notification summary to debug output
+				//System.Diagnostics.Debug.WriteLine(summary);
+			};
+			//生成CustomID
+			var installId = MobileCenter.GetInstallIdAsync();
+			System.Diagnostics.Debug.WriteLine("installId value:" + installId.Result.ToString());
+
 			MobileCenter.Start("9d6d8829-627c-4ceb-815e-90ba99958eb8",
 				   typeof(Analytics), typeof(Crashes));
 			// Minimal basic setup without a storyboard.
